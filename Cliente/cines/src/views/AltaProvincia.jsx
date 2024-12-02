653b6615e516d4e9fe92b0345a3adba5d2238d2c
FELIPE PIANELLI <fpianelli@alu.frlp.utn.edu.ar>
2024-12-02 11:50:10 -0300

    hicimos altas de algunas cositas

--- Cliente/cines/src/views/AltaProvincia.jsx
+++ Cliente/cines/src/views/AltaProvincia.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React from 'react'
 import { Form, Container, Row, Col } from 'react-bootstrap'
 import GuardarBtn from '../components/buttons/GuardarBtn';
 import CancelarBtn from '../components/buttons/CancelarBtn';
 import { useNavigate, useLocation } from 'react-router-dom';
+import { createProvincia } from '../helpers/provincia/provinciaService'
+import { useForm } from 'react-hook-form';
 
 const AltaProvincia = () => {
+    const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm();
     const navigate = useNavigate(); 
     const location = useLocation(); 
+
     const handleCancel = () => {
         if (location.state?.from === 'domicilio') {
             navigate('/altaCine'); 
@@ -15,21 +19,39 @@ const AltaProvincia = () => {
         }
     };
 
+    const onSubmit = async (data) => {
+        try {
+            const response = await createProvincia(data); 
+            console.log('Provincia creada exitosamente:', response);
+            if (location.state?.from === 'domicilio') {
+                navigate('/altaCine'); 
+            } else {
+                navigate('/'); 
+            }
+        } catch (error) {
+            console.error('Error al crear provincia:', error);
+        }
+    };
+
     return (
         <div className='d-flex justify-content-center align-items-center w-100' style={{ height: '100vh' }}>
             <div className='d-flex flex-column align-items-center'>
                 <Container className="mt-4">
-                    <Form>
+                    <Form onSubmit={handleSubmit(onSubmit)}>
                         <Row className="mb-4">
                             <Col md={12}>
-                                <Form.Group controlId="formName">
+                                <Form.Group controlId="provinciaId">
                                     <Form.Label>Nombre</Form.Label>
-                                    <Form.Control type="text" />
+                                    <Form.Control
+                                        type="text" 
+                                        placeholder="nombre provincia"
+                                        {...register('nombre', { required: 'El nombre es obligatorio' })} 
+                                    />
                                 </Form.Group>
                             </Col>
                         </Row>
                         <div className="d-flex justify-content-end gap-2">
-                            <GuardarBtn/>
+                            <GuardarBtn type="submit" disabled={isSubmitting}/>
                             <CancelarBtn onClick={handleCancel}/>
                         </div>
                     </Form>
--- Cliente/cines/src/views/AltaTurno.jsx
+++ Cliente/cines/src/views/AltaTurno.jsx
@@ -3,10 +3,14 @@ import { Form, Button, Container, Row, Col } from 'react-bootstrap'
 import GuardarBtn from '../components/buttons/GuardarBtn';
 import CancelarBtn from '../components/buttons/CancelarBtn';
 import { useNavigate, useLocation } from 'react-router-dom';
+import { createTurno } from '../helpers/turno/turnoService'
+import { useForm } from 'react-hook-form';
 
 const AltaTurno = () => {
     const navigate = useNavigate(); 
     const location = useLocation(); 
+    const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm();
+
     const handleCancel = () => {
         if (location.state?.from === 'turnos') {
             navigate('/altaCine'); 
@@ -15,22 +19,40 @@ const AltaTurno = () => {
         }
     };
 
+    const onSubmit = async (data) => {
+        try {
+            const response = await createTurno(data); 
+            console.log('Turno creado exitosamente:', response);
+            if (location.state?.from === 'turnos') {
+                navigate('/altaCine'); 
+            } else {
+                navigate('/'); 
+            }
+        } catch (error) {
+            console.error('Error al crear turno:', error);
+        }
+    };
+
     return (
         <div className='d-flex justify-content-center align-items-center w-100' style={{ height: '100vh' }}>
             <div className='d-flex flex-column align-items-center'>
                 <Container className="mt-4">
-                    <Form>
+                    <Form onSubmit={handleSubmit(onSubmit)}>
                         <Row className="mb-4">
                             <Col md={12}>
                                 <Form.Group controlId="formName">
                                     <Form.Label>Nombre del turno</Form.Label>
-                                    <Form.Control type="text" />
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="tipo turno"
+                                        {...register('tipo', { required: 'El tipo del turno es obligatorio' })} 
+                                    />
                                 </Form.Group>
                             </Col>
                         </Row>
 
                         <div className="d-flex justify-content-end gap-2">
-                            <GuardarBtn/>
+                            <GuardarBtn type="submit" disabled={isSubmitting}/>
                             <CancelarBtn onClick={handleCancel}/>
                         </div>
     
